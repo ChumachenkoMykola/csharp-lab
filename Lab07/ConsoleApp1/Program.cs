56d13bb0f1017c3cab39a7af3b9f11e26458672e
Mykola <mykola.chumachenko@microsoft.wsei.edu.pl>
2024-12-10 10:16:43 +0100

    Lab07

---
+++ Lab07/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal animal = new Dog();
+            Console.WriteLine(animal.Sound);
+            animal.Move();
+
+            Dog dog1 = new Dog();
+            Console.WriteLine(dog1.Sound);
+            dog1.Move();
+
+            (animal as IWalkable).Walk();
+
+            Dog dog2 = new Dog();
+            dog2.Walk();
+
+            IWalkable walkableDog = new Dog();
+            walkableDog.Walk();
+
+
+            Animal[] animals = new Animal[] { new Cat(), new Dog() };
+
+            foreach (Animal ani in animals)
+            {
+                Console.WriteLine($"The {ani.GetType().Name} goes {ani.Sound}");
+                animal.Move();
+            }
+        }
+    }
+}
